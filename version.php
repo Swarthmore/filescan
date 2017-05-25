77789f8697b42d3f86d3295387cfdb2bf56d360e
aruethe2 <aruethe2@swarthmore.edu>
2017-05-25 10:44:59 -0400

    Fix typo

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
-i$plugin->component = 'block_pdfcheck'; // Declare the type and name of this plugin.
+$plugin->component = 'block_pdfcheck'; // Declare the type and name of this plugin.
 $plugin->version = 2015101603;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2014111007; // YYYYMMDDHH (Moodle version)
