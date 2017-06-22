80e495b5700bb8d15f732eeca13236a67180ef54
Andrew Ruether <aruethe2@swarthmore.edu>
2017-06-22 16:43:12 -0400

    Rewrite to work with local filescan plugin

---
+++ lang/en/block_filescan.php
@@ -0,0 +1,11 @@
+<?php
+$string['pluginname'] = 'File Scan';
+$string['filescan'] = 'File Scan';
+$string['addinstance'] = 'Add a new File Scan block';
+$string['viewpages'] = 'View File Scan';
+$string['managepages'] = 'Manage File Scan';
+$string['viewdetailspage'] = 'View File Details';
+
+
+// Cache
+$string['cachedef_filescan'] = 'Filescan results cache for each course';
--- lang/en/block_pdfcheck.php
+++
@@ -1,7 +0,0 @@
-<?php
-$string['pluginname'] = 'PDF Check';
-$string['pdfcheck'] = 'PDF Check';
-$string['addinstance'] = 'Add a new PDF Check block';
-$string['viewpages'] = 'View PDF Checker';
-$string['managepages'] = 'Manage PDF Checker';
-$string['viewdetailspage'] = 'View Scan Details';
