2b418812dc0fb651515e0f45288faed3aa8f9563
Andrew Ruether <aruethe2@swarthmore.edu>
2017-07-13 12:09:49 -0400

    Update minimum Moodle to 3.2 -- to require PHP 5.5+

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
 $plugin->component = 'block_filescan'; // Declare the type and name of this plugin.
-$plugin->version = 2017062302;  // YYYYMMDDHH (year, month, day, 24-hr time)
-$plugin->requires = 2016010100; // YYYYMMDDHH (Moodle version)
+$plugin->version = 20170713012;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->requires = 2016120500; // YYYYMMDDHH (Moodle version)
