c077e5f9463a61a5820f8bd87a75a04c6b74215b
aruethe2 <aruethe2@swarthmore.edu>
2017-07-14 10:10:10 -0400

    Backed Moodle version requirement to 3.1

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
 $plugin->component = 'block_filescan'; // Declare the type and name of this plugin.
 $plugin->version = 20170713012;  // YYYYMMDDHH (year, month, day, 24-hr time)
-$plugin->requires = 2016120500; // YYYYMMDDHH (Moodle version)
+$plugin->requires = 2016052300; // YYYYMMDDHH (Moodle version)
