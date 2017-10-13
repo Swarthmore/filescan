e0328f807197a637f5eddcc9808b1c091d0fd71f
Andrew Ruether <aruethe2@swarthmore.edu>
2017-10-13 18:16:55 -0400

    Course info added to filescan table

--- classes/task/generate_report.php
+++ classes/task/generate_report.php
@@ -44,81 +44,93 @@ class generate_report extends \core\task\scheduled_task {
 		
         mtrace( "Acessibility report generation script is running" );
 
+		// Start by clearing out the old couseinfo entries.  They could contain information that is outdated
+		$DB->execute("update {block_filescan_files} set courseinfo=NULL");
        
         // Select all PDF files in the system
-        $query = 'select * from {files} f, {context} ctx where
+        $query = 'select f.contenthash as contenthash, ctx.path as path, fs.id as filescanid, fs.courseinfo as courseinfo from {files} f, {context} ctx, {block_filescan_files} fs where
 f.component in ("course", "course", "block_html", "mod_assign", "mod_book", "mod_data", "mod_folder", "mod_forum", "mod_glossary","mod_label", "mod_lesson", "mod_page", "mod_publication", "mod_questionnaire", "mod_quiz", "mod_resource", "mod_scorm", "mod_url", "mod_workshop", "qtype_essay", "question") AND f.filesize <> 0  
 	AND f.mimetype = "application/pdf"
 	AND f.contextid = ctx.id
-	ORDER BY f.timemodified DESC limit 100';
+	AND f.contenthash = fs.contenthash';
         		
         // Get a number of records as a moodle_recordset using a SQL statement.		
 		$pdf_rs = $DB->get_recordset_sql($query);
 		  
-		if ($pdf_rs->valid()) {
-    		// The recordset contains records.
-    		
-    		foreach($pdf_rs as $key => $value) {
-    		
-    			$course_object = new \stdClass();
-    		
-    			mtrace(print_r($key,true));
-    			mtrace(print_r($value,true));
-    			
-    			// Get an array of contexts for this file.  Look for the one that is a course
-    			$contexts = array_reverse(explode("/",$value->path));
-    			array_shift($contexts);
-    			
-    			list($insql, $params) = $DB->get_in_or_equal($contexts);
-				$sql = "select * from {context} where id $insql";
-				$context_results = $DB->get_records_sql($sql, $params);
-    			
-    			$courseid = NULL;
-    			foreach ($context_results as $c) {
-    				// Find the context where contextlevel = 50 (course)
-        			if ($c->contextlevel == 50) {
-        				$courseid = $c->instanceid;
-        				break;
-        			}
-        		}
-        		
-        		// Lookup course info
-        		if ($courseid) {
-					$context = \context_course::instance($courseid);
-					$course = get_course($courseid);	
-					$num_users = count(get_enrolled_users($context,'',0));
-					$teachers = get_role_users($teacher_role_id, $context,NULL , "u.id, u.firstname, u.lastname, u.email");
-					
-					$teacher_string = "";
-					foreach($teachers as $t) {
-						$teacher_string .= sprintf("%s %s, ", $t->firstname, $t->lastname);
-					}
-					$teacher_string = substr($teacher_string, 0, -2);
-					
-					$course_object->fullname = $course->fullname;
-					$course_object->shortname = $course->shortname;
-					$course_object->courseid = $courseid;
-					$course_object->teachers = $teachers;
-					$course_object->student_enrollment = $num_users - count($teachers);
-					
-					
-					mtrace(serialize($course_object));
-				}
-
-    			echo "\n";
-			}
-    		
-    		
-		} else {
-		    mtrace("No files found");
+		if (!$pdf_rs->valid()) {
+			mtrace("No files found");
             return false;
-		}
+        }
+    		
+		// The recordset contains records.
+		$index = 0;
+		foreach($pdf_rs as $key => $file) {
+		
+			$course_object = new \stdClass();
+				
+			// Get an array of contexts for this file.  Look for the one that is a course
+			$contexts = array_reverse(explode("/",$file->path));
+			array_shift($contexts);
+			
+			list($insql, $params) = $DB->get_in_or_equal($contexts);
+			$sql = "select * from {context} where id $insql";
+			$context_results = $DB->get_records_sql($sql, $params);
+			
+			$courseid = NULL;
+			foreach ($context_results as $c) {
+				// Find the context where contextlevel = 50 (course)
+				if ($c->contextlevel == 50) {
+					$courseid = $c->instanceid; 
+					break;
+				}
+			}
+			
+			// If no course information is available, skip to the next file
+			if (!$courseid) {
+				continue;
+			}
 			
 			
-		$pdf_rs->close();
-		
-		
+			// Lookup course info for this entry
+			// Build a course object than can be serialized.
+			// Course object contains course name, enrollment info, and teachers.
+			$context = \context_course::instance($courseid);
+			$course = get_course($courseid);	
+			$num_users = count(get_enrolled_users($context,'',0));
+			$teachers = get_role_users($teacher_role_id, $context,NULL , "u.id, u.firstname, u.lastname, u.email");
+			
+			$course_object->fullname = $course->fullname;
+			$course_object->shortname = $course->shortname;
+			$course_object->courseid = $courseid;
+			$course_object->teachers = $teachers;
+			$course_object->student_enrollment = $num_users - count($teachers);
+				
+			$courseinfo = [];
+			if ($file->courseinfo) {
+				// There is already a course info entry for this file.  
+				$courseinfo = json_decode($file->courseinfo);
+			}
 
+			
+			// Add the new course info to the end of the array
+			$courseinfo[] = $course_object;
+			
+			// Save the updated course info back to the filescan table
+			$update_filescan = $DB->update_record('block_filescan_files', array('id'=>$file->filescanid, 'courseinfo'=>json_encode($courseinfo)), false);
+						
+			$index += 1;
+			
+			if (($index % 1000) == 0) {
+				mtrace("Processed ". $index . " files");
+			}
+			
+		}
+    		
+		
+		$pdf_rs->close();
+		if (($index % 1000) != 0) {
+			mtrace("Processed ". $index . " files");
+		}
  	}       
 
 }
--- db/upgrade.php
+++ db/upgrade.php
@@ -97,9 +97,6 @@ function xmldb_block_filescan_upgrade($oldversion) {
         upgrade_block_savepoint(true, 2017071414, 'filescan');
     }
 
-
-
-
     /// Add a new column newcol to the mdl_myqtype_options
     if ($oldversion < 2017082410) {
 
@@ -117,7 +114,50 @@ function xmldb_block_filescan_upgrade($oldversion) {
     }
 
 
+    if ($oldversion < 2017101308) {
 
+        // Define field courseinfo to be added to block_filescan_files.
+        $table = new xmldb_table('block_filescan_files');
+        $field = new xmldb_field('courseinfo', XMLDB_TYPE_TEXT, null, null, null, null, null, 'timechecked');
+
+        // Conditionally launch add field courseinfo.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Filescan savepoint reached.
+        upgrade_plugin_savepoint(true, 2017101308, 'error', 'filescan');
+    }
+
+    if ($oldversion < 2017101310) {
+
+        // Define field timechecked to be added to block_filescan_files.
+        $table = new xmldb_table('block_filescan_files');
+        $field = new xmldb_field('timecourseinfo', XMLDB_TYPE_DATETIME, null, null, null, null, '0', 'courseinfo');
+
+        // Conditionally launch add field timechecked.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Filescan savepoint reached.
+        upgrade_plugin_savepoint(true, 2017101310, 'error', 'filescan');
+    }
+
+   if ($oldversion < 2017101318) {
+
+        // Define field timecourseinfo to be dropped from block_filescan_files.
+        $table = new xmldb_table('block_filescan_files');
+        $field = new xmldb_field('timecourseinfo');
+
+        // Conditionally launch drop field timecourseinfo.
+        if ($dbman->field_exists($table, $field)) {
+            $dbman->drop_field($table, $field);
+        }
+
+        // Filescan savepoint reached.
+        upgrade_plugin_savepoint(true, 2017101318, 'error', 'filescan');
+    }
 
     return true;
 } 
