ce99423812e8a66b58ccb978f78454b8bd93610d
Anthony Weed <aweed1@swarthmore.edu>
2018-09-19 10:00:05 -0400

    new branch

--- block_filescan.php
+++ block_filescan.php
@@ -4,84 +4,82 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
 
-
-
-
 class block_filescan extends block_base {
-
-
-
-    public function init() {
-        $this->title = get_string('filescan', 'block_filescan');
-    }
-
-    public function applicable_formats() {
-        return array('site' => false, 'my' => false, 'course' => true);
-    }
-
-private function get_file_list() {
-
-	global $COURSE, $CFG, $DB;
-	require_once($CFG->dirroot . '/course/lib.php');
-
-	$cms = get_fast_modinfo($COURSE)->get_cms();
-	$file_list = array();	// Array containing detailed file info
-	$fs = get_file_storage();
 	
-	
-	// Loop through each module in the course looking for files
-	foreach ($cms as $cm) {
-
-		if ($cm->is_user_access_restricted_by_capability()) {
-			continue;
-		}
-	
-		$cmtype = $cm->modname;
-		$module_name = $cm->name;
-	
-		$section_no = $cm->get_course_module_record(true)->sectionnum;
-		$sectionName = get_section_name($COURSE->id, $section_no);
-	
-		// if resource is a folder
-		if ($cmtype === 'folder') {
-			$cmfiles = $fs->get_area_files($cm->context->id, 'mod_folder', 'content', false, 'timemodified', false);
-
-			// Loop through all files in a folder
-			foreach ($cmfiles as $f) {
-		
-				if (isset($f) && ($f->get_mimetype() === 'application/pdf')) {
-					array_push($file_list, $f->get_contenthash());					
-				}
-			}
-		
-		} else if ($cmtype === 'resource') { // if resource is a file.
-		
-			// Get files in "file" resource 
-			$files = $fs->get_area_files($cm->context->id, 'mod_resource', 'content', false, 'timemodified', false);
-		
-			// Loop through each file
-			foreach ($files as $f) {
-				if (isset($f) && ($f->get_mimetype() === 'application/pdf')) {
-					array_push($file_list, $f->get_contenthash());						
-				}
-			}
-		}	
+	public function init() {
+		$this->title = get_string('filescan', 'block_filescan');
 	}
 	
-	return $file_list;
-
-}
-
-
-  	private function generate_summary($courseid) {
-		global $DB;
+	public function applicable_formats() {
+		// sitewide			dashboard				per course
+		return array('site' => true, 'my' => true, 'course' => true);
+	}
 	
-		$filelist = $this->get_file_list();
+	// when you enter a course, this pulls the files for the selected course
+	// there's a caching mechanism for this -- this is only called when there's
+	// not a cache summary in place
+	
+	private function get_course_files() {
+		
+		global $COURSE, $CFG, $DB;
+		require_once($CFG->dirroot . '/course/lib.php');
+		
+		$cms = get_fast_modinfo($COURSE)->get_cms();
+		$file_list = array();	// Array containing detailed file info
+		$fs = get_file_storage();
+		
+		// Loop through each module in the course looking for files
+		foreach ($cms as $cm) {
+			
+			if ($cm->is_user_access_restricted_by_capability()) {
+				continue;
+			}
+			
+			$cmtype = $cm->modname;
+			$module_name = $cm->name;
+			
+			$section_no = $cm->get_course_module_record(true)->sectionnum;
+			$sectionName = get_section_name($COURSE->id, $section_no);
+			
+			// if resource is a folder
+			if ($cmtype === 'folder') {
+				$cmfiles = $fs->get_area_files($cm->context->id, 'mod_folder', 'content', false, 'timemodified', false);
+				
+				// Loop through all files in a folder
+				foreach ($cmfiles as $f) {
+					if (isset($f) && ($f->get_mimetype() === 'application/pdf')) {
+						array_push($file_list, $f->get_contenthash());					
+					}
+				}
+				
+			} else if ($cmtype === 'resource') { // if resource is a file.
+				
+				// Get files in "file" resource 
+				$files = $fs->get_area_files($cm->context->id, 'mod_resource', 'content', false, 'timemodified', false);
+				
+				// Loop through each file
+				foreach ($files as $f) {
+					if (isset($f) && ($f->get_mimetype() === 'application/pdf')) {
+						array_push($file_list, $f->get_contenthash());						
+					}
+				}
+			}	
+		}
+		
+		return $file_list;
+		
+	}
+	
+	
+	private function generate_summary($courseid) {
+		global $DB;
+		
+		$filelist = $this->get_course_files();
 		$accessible = 0;
 		$partially_accessible = 0;
 		$inaccessible = 0;
 		$unknown = 0;
-	
+		
 		foreach($filelist as $f) {
 			// For each file, lookup file scan status
 			$record = $DB->get_record("block_filescan_files", array('contenthash'=>$f));
@@ -95,7 +93,7 @@ private function get_file_list() {
 				$unknown = $unknown + 1;
 			}
 		}
-	
+		
 		$output = get_string('summary:files_found', 'block_filescan', count($filelist));
 		
 		if ($accessible > 0) {
@@ -118,74 +116,62 @@ private function get_file_list() {
 		
 		return $output;
 	}
+	
+	/**
+	 * Moodle calls this to display the content in the block
+	 */
 
+	public function get_content() {
+		
+		global $COURSE, $CFG, $DB;
+		require_once($CFG->dirroot . '/course/lib.php');
+		
+		$systemcontext = context_system::instance();
 
+		var_dump($systemcontext);
+		
+		$context = context_course::instance($COURSE->id);
+		$canview = has_capability('block/filescan:viewpages', $context);
+		
+		// If the role can't view the block, return an empty string for the content
+		// and exit (blocks with no content aren't shown)
+		if 	(!$canview) {
+			$this->content =  new stdClass;
+			$this->content->text   = "";
+			$this->content->footer = "";
+			return $this->content;	
+		}
+		
+		// Determine course metadata
+		$coursename = $COURSE->fullname;
+		$courseshortname = $COURSE->shortname;
+		$courseurl = course_get_url($COURSE);
+		
+		$cache = cache::make('block_filescan', 'filescan');
+		$filescan_cache = $cache->get($COURSE->id);
+		if ($filescan_cache) {
+			// Cache was found.  Print out summary
+			$filescan_summary = $filescan_cache;
+		} else {
+			// No cache data found.  Generate summary and save to cache
+			$filescan_summary = $this->generate_summary($COURSE->id);
+			$result = $cache->set($COURSE->id, $filescan_summary);
+		}		
 
-
-
-
-
-
-
-
-public function get_content() {
-
-	global $COURSE, $CFG, $DB;
-	require_once($CFG->dirroot . '/course/lib.php');
-
- 	$context = context_course::instance($COURSE->id);
- 	$canview = has_capability('block/filescan:viewpages', $context);
- 	
- 
- 	// If the role can't view the block, return an empty string for the content
-	// and exit (blocks with no content aren't shown)
-	if 	(!$canview) {
-	    $this->content =  new stdClass;
-    	$this->content->text   = "";
-		$this->content->footer = "";
-     	return $this->content;	
+		if ($this->content !== null) {
+			return $this->content ;
+		}
+		
+		$this->content         =  new stdClass;
+		$this->content->text   = '<h4>Summary</h4>' . $filescan_summary;
+		
+		$url = new moodle_url('/blocks/filescan/view.php', array('courseid' => $COURSE->id));
+		$this->content->footer = html_writer::link($url, get_string('viewdetailspage', 'block_filescan'));
+		
+		return $this->content;
 	}
- 	
+	
+	// Tell Moodle there is a global config
+	function has_config() {return true;}
 
- 	
- 	// Determine course metadata
-	$coursename = $COURSE->fullname;
-    $courseshortname = $COURSE->shortname;
-    $courseurl = course_get_url($COURSE);
-
-		
- 	$cache = cache::make('block_filescan', 'filescan');
- 	$filescan_cache = $cache->get($COURSE->id);
- 	if ($filescan_cache) {
- 		// Cache was found.  Print out summary
- 		$filescan_summary = $filescan_cache;
- 	} else {
- 		// No cache data found.  Generate summary and save to cache
- 		$filescan_summary = $this->generate_summary($COURSE->id);
- 		$result = $cache->set($COURSE->id, $filescan_summary);
- 	}		
-		
-
-
-    if ($this->content !== null) {
-      return $this->content ;
-    }
-    
-
-    $this->content         =  new stdClass;
-    $this->content->text   = '<h4>Summary</h4>' . $filescan_summary;
-    
-	$url = new moodle_url('/blocks/filescan/view.php', array('courseid' => $COURSE->id));
-	$this->content->footer = html_writer::link($url, get_string('viewdetailspage', 'block_filescan'));
-
- 
-    return $this->content;
-  }
-  
-  
-  	// Tell Moodle there is a global config
- 	function has_config() {return true;}
-  
-  
-  
-}   // Here's the closing bracket for the class definition
+}
--- view.php
+++ view.php
@@ -4,18 +4,16 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
 
- 
 require_once('../../config.php');
 global $DB;
- 
+
 // Check for all required variables.
 $courseid = required_param('courseid', PARAM_INT);
 
 if (!$course = $DB->get_record('course', array('id' => $courseid))) {
-    print_error('invalidcourse', 'block_filescan', $courseid);
+	print_error('invalidcourse', 'block_filescan', $courseid);
 }
- 
- 
+
 require_login($course);
 $PAGE->set_url('/block/filescan/view.php', array('id' => $courseid));
 $PAGE->set_pagelayout('standard');
@@ -23,12 +21,10 @@ $PAGE->set_heading(get_string('viewdetailspage', 'block_filescan'));
 
 global $COURSE, $CFG, $DB, $OUTPUT, $PAGE;
 
-
 require_once($CFG->dirroot . '/course/lib.php');
 $fs = get_file_storage();
 $context = context_course::instance($COURSE->id);
 $canview = has_capability('block/filescan:viewpages', $context);
-
 
 // If the role can't view the block, return an empty string for the content
 // and exit (blocks with no content aren't shown)
@@ -36,12 +32,11 @@ if 	(!$canview) {
 	echo get_string('cannot_view', 'block_filescan');
 	return;
 }
- 		
+
 // Clear any existing cache data for this course 
 $cache = cache::make('block_filescan', 'filescan');
 $filescan_cache = $cache->delete($COURSE->id); 		
- 		
- 		
+
 // Determine course metadata
 $coursename = $COURSE->fullname;
 $courseshortname = $COURSE->shortname;
@@ -52,18 +47,14 @@ $output_html = "";
 $file_listing = [];
 $cmid = null;
 
-
-
 // Get any existing data from db
-
 $cms = ($cmid === null) ? get_fast_modinfo($COURSE)->get_cms() : array(get_fast_modinfo($COURSE)->get_cm($cmid));
-
 
 $file_list = array();	// Array containing detailed file info
 
 // Loop through each module in the course looking for files
 foreach ($cms as $cm) {
-
+	
 	if ($cm->is_user_access_restricted_by_capability()) {
 		continue;
 	}
@@ -77,10 +68,10 @@ foreach ($cms as $cm) {
 	// if resource is a folder
 	if ($cmtype === 'folder') {
 		$cmfiles = $fs->get_area_files($cm->context->id, 'mod_folder', 'content', false, 'timemodified', false);
-
+		
 		// Loop through all files in a folder
 		foreach ($cmfiles as $f) {
-		
+			
 			if (isset($f) && ($f->get_mimetype() === 'application/pdf')) {
 				$file_item = create_file_item($DB, $cm, $cmtype, $module_name, $sectionName, $section_no, $f);
 				array_push($file_list, $file_item);					
@@ -103,10 +94,6 @@ foreach ($cms as $cm) {
 	}	
 }
 
-
-
-
-		
 // Loop through PDF listing to format the output into a table
 
 $success_icon = '<i class="fa fa-check text-success fa-fw" aria-hidden="true"></i>';
@@ -151,87 +138,87 @@ foreach ($file_list as $f) {
 		$output_html .= get_table_header('outline_check');
 		$output_html .= "</tr>";
 	}
-
+	
 	switch($f['status']['hastext']) {
 		case -1:
-			$hastext = "&#10071";
-			break;			
+		$hastext = "&#10071";
+		break;			
 		case "1":
-			$hastext = $success_icon;		// Yes
-			break;			
+		$hastext = $success_icon;		// Yes
+		break;			
 		case "0":
-			$hastext = $fail_icon; 	// No
-			break;			
+		$hastext = $fail_icon; 	// No
+		break;			
 		default:
-			$hastext = $unknown_icon;	// Unknown
+		$hastext = $unknown_icon;	// Unknown
 	}
-
+	
 	switch($f['status']['hastitle']) {
 		case -1:
-			$hastitle = "&#10071";
-			break;			
+		$hastitle = "&#10071";
+		break;			
 		case "1":
-			$hastitle = $success_icon;		// Yes
-			break;			
+		$hastitle = $success_icon;		// Yes
+		break;			
 		case "0":
-			$hastitle = $fail_icon; 	// No
-			break;			
+		$hastitle = $fail_icon; 	// No
+		break;			
 		default:
-			$hastitle = $unknown_icon;	// Unknown
+		$hastitle = $unknown_icon;	// Unknown
 	}
-		
+	
 	switch($f['status']['haslanguage']) {
 		case -1:
-			$haslanguage = "&#10071";
-			break;			
+		$haslanguage = "&#10071";
+		break;			
 		case "1":
-			$haslanguage = $success_icon;		// Yes
-			break;			
+		$haslanguage = $success_icon;		// Yes
+		break;			
 		case "0":
-			$haslanguage = $fail_icon; 	// No
-			break;			
+		$haslanguage = $fail_icon; 	// No
+		break;			
 		default:
-			$haslanguage = $unknown_icon;	// Unknown
+		$haslanguage = $unknown_icon;	// Unknown
 	}		
 	
 	switch($f['status']['hasoutline']) {
 		case -1:
-			$hasoutline = "&#10071";
-			break;			
+		$hasoutline = "&#10071";
+		break;			
 		case "1":
-			$hasoutline = $success_icon;		// Yes
-			break;			
+		$hasoutline = $success_icon;		// Yes
+		break;			
 		case "0":
-			$hasoutline = $fail_icon; 	// No
-			break;			
+		$hasoutline = $fail_icon; 	// No
+		break;			
 		default:
-			$hasoutline = $unknown_icon;	// Unknown
+		$hasoutline = $unknown_icon;	// Unknown
 	}	
-		
+	
 	# Determine overall status
 	switch($f['status']['status']) {
 		case "fail":
-			$overallstatus = $fail_icon; 	// Bad
-			break;			
+		$overallstatus = $fail_icon; 	// Bad
+		break;			
 		case "pass":
-			$overallstatus = $success_icon;		// Good 
-			break;			
+		$overallstatus = $success_icon;		// Good 
+		break;			
 		case "check":
-			$overallstatus = $partial_success_icon; 	// check
-			break;			
+		$overallstatus = $partial_success_icon; 	// check
+		break;			
 		default:
-			$overallstatus = $unknown_icon;	// Unknown
+		$overallstatus = $unknown_icon;	// Unknown
 	}	
 	
 	
-					
+	
 	// Check to see if this is a folder
 	if ($f['mod_type'] == 'folder') {
 		$mod_link = "<a href=\"" . $f['mod_url'] . "\" title=\"" . $f['mod_name'] . "\">&#128194;</a>" ;
 	} else {
 		$mod_link = "<a href=\"" . $f['mod_url'] . "\" title=\"" . $f['mod_name'] . "\">&#128196;</a>" ;
 	}				
-
+	
 	$output_html .= "<tr><td style='text-align:center'>" . $mod_link ."<td><a href=\"" . $f['fileurl']  . "\" target='_blank'>" . $f['filename'] . "</a></td>
 	<td style='text-align:center;'><span title=\"" . get_string('table:status_header', 'block_filescan') . '">' . $overallstatus . "</span></td>
 	<td style='text-align:center;border-left-width:2px;'><span title=\"" . get_string('table:text_check_header', 'block_filescan') . '">' . $hastext . "</span></td>
@@ -244,33 +231,29 @@ foreach ($file_list as $f) {
 }
 $output_html .= "</tbody></table>"; // Close out last table on last section
 
-
-
-
 // Calculate summary information
-
 $processing = false;	// Assume no files are still processing.
 
 $ocr_summary = array ('Accessible' => 0, 'Partially Accessible' => 0, 'Inaccessible' => 0, 'Pending' => 0, 'Error' => 0, 'Unknown' => 0);
 foreach ($file_list as $f) {
 	switch($f['status']['status']) {
 		case "pending":
-			$ocr_summary['Pending']++;
-			break;
+		$ocr_summary['Pending']++;
+		break;
 		case "error":
-			$ocr_summary['Error']++;
-			break;			
+		$ocr_summary['Error']++;
+		break;			
 		case "pass":
-			$ocr_summary['Accessible']++;
-			break;			
+		$ocr_summary['Accessible']++;
+		break;			
 		case "fail":
-			$ocr_summary['Inaccessible']++;
-			break;	
+		$ocr_summary['Inaccessible']++;
+		break;	
 		case "check":
-			$ocr_summary['Partially Accessible']++;
-			break;							
+		$ocr_summary['Partially Accessible']++;
+		break;							
 		default:
-			$ocr_summary['Unknown']++;
+		$ocr_summary['Unknown']++;
 	}
 }
 
@@ -308,50 +291,45 @@ if ($ocr_summary['Unknown'] > 0) {
 
 // Prepend summary to top of content
 $output_html = "<h1>" . get_string('summary:title', 'block_filescan') . "</h1>" . $summary_text . "<BR><BR>" . $output_html;
-			
 
 echo $OUTPUT->header();
 echo $output_html;
 echo $OUTPUT->footer();
 
-
-
-
-
 // From https://github.com/omer-ilhan/moodle-block_files/blob/master/block_files.php
 /**
-     * Create a file item.
-     *
-     * @param $coursename string name of the course this file item belongs to
-     * @param $courseshortname string short name of the course this file item belongs to
-     * @param $courseurl url of the course this file item belongs to
-     * @param $cm course module this file item belongs to
-     * @param $cmfile the actual file
-     * @return array describing a file item, used to create table items
-     */
-     
- function create_file_item($DB, $cm, $cmtype, $mod_name, $sectionName, $section_no, $file) {
- 
- 	// See if there is an file scan entry for this file
- 	
-		$results = $DB->get_record("block_filescan_files", array('contenthash'=>$file->get_contenthash()), $fields='*');
- 	 	
- 	 	// Assume no status -- correct if it exists
- 	 	
- 		if ( $results){
- 			$status = array(
- 				"checked"=>($results->checked),
- 				"status"=>($results->status),
- 				"hastext"=>($results->hastext),
- 				"hastitle"=>($results->hastitle),
- 				"haslanguage"=>($results->haslanguage),
- 				"hasoutline"=>($results->hasoutline),
- 				"pagecount"=>($results->pagecount)
- 			);
- 		} else {
- 			$status = null;
- 		}
- 
+* Create a file item.
+*
+* @param $coursename string name of the course this file item belongs to
+* @param $courseshortname string short name of the course this file item belongs to
+* @param $courseurl url of the course this file item belongs to
+* @param $cm course module this file item belongs to
+* @param $cmfile the actual file
+* @return array describing a file item, used to create table items
+*/
+
+function create_file_item($DB, $cm, $cmtype, $mod_name, $sectionName, $section_no, $file) {
+	
+	// See if there is an file scan entry for this file
+	
+	$results = $DB->get_record("block_filescan_files", array('contenthash'=>$file->get_contenthash()), $fields='*');
+	
+	// Assume no status -- correct if it exists
+	
+	if ( $results){
+		$status = array(
+			"checked"=>($results->checked),
+			"status"=>($results->status),
+			"hastext"=>($results->hastext),
+			"hastitle"=>($results->hastitle),
+			"haslanguage"=>($results->haslanguage),
+			"hasoutline"=>($results->hasoutline),
+			"pagecount"=>($results->pagecount)
+		);
+	} else {
+		$status = null;
+	}
+	
 	return array(
 		'mod_id' => $cm->id,
 		'mod_type' => $cmtype,
@@ -369,7 +347,5 @@ echo $OUTPUT->footer();
 		'file' => $file
 	);
 }
-
-
 
 ?>
