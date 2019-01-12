4fa6b2a585516a4f6c3b6572a4d19ad99955b200
Anthony Weed <aweed1@swarthmore.edu>
2019-01-11 20:00:54 -0500

    fixed shim

--- amd/src/config.js
+++ amd/src/config.js
@@ -2,7 +2,7 @@ define([], function(){
   window.requirejs.config({
     paths: {
       // Paths to required js libraries
-      "DataTables": M.cfg.wwwroot + "/../lib/datatables.min.js"
+      "DataTables": M.cfg.wwwroot + "/blocks/filescan/amd/lib/datatables.min.js"
     },
     shim: {
       // The names that will be used to refer to lib
