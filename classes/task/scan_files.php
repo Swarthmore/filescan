a0fc41031e8850bd6ac667020f9cd4ebef73bd51
tony <aweed1@swarthmore.edu>
2019-10-03 15:39:59 -0400

    Implements a refactor or scan_files.php Implements curl requests through moodle curl

--- classes/task/scan_files.php
+++ classes/task/scan_files.php
@@ -13,117 +13,147 @@ namespace block_filescan\task;
 class scan_files extends \core\task\scheduled_task
 {
 
+  /**
+   * @description This is a helper function that returns the name of the filescan task
+   * @return string
+   */
   public function get_name()
   {
-    // Shown in admin screens
     return get_string('filescan_task', 'block_filescan');
   }
 
-
-  private static function save_filescan_results($DB, $fileentry)
+  private static function save_filescan_results($conn, $row)
   {
-
-    $record = $DB->get_record("block_filescan_files", array('contenthash' => $fileentry->contenthash));
+    $record = $conn->get_record("block_filescan_files", array('contenthash' => $row->contenthash));
 
     if ($record) {
-      $fileentry->id = $record->id;
+      $row->id = $record->id;
 
       // If this is a failure, update the statuscode
-      if ($fileentry->checked == 0) {
-        $fileentry->statuscode = $record->statuscode + 1;
+      if ($row->checked == 0) {
+        $row->statuscode = $record->statuscode + 1;
       }
-      $sql = $DB->update_record("block_filescan_files", $fileentry);
-      $sql ? mtrace("Updated record for " . $fileentry->contenthash) : mtrace("Could not update record " . $fileentry->contenthash);
+
+      $sql = $conn->update_record("block_filescan_files", $row);
+      $sql
+        ? mtrace("Updated record for " . $row->contenthash)
+        : mtrace("Could not update record " . $row->contenthash);
 
     } else {
-      if ($fileentry->checked == 0) {
-        $fileentry->statuscode = 1;
+
+      if ($row->checked == 0) {
+        $row->statuscode = 1;
       }
 
-      $sql = $DB->insert_record("block_filescan_files", $fileentry, $returnid = true, $bulk = false);
+      $sql = $conn->insert_record("block_filescan_files", $row, $returnid = true, $bulk = false);
       $sql ? mtrace("Inserted record") : mtrace("Could not insert record");
     }
   }
 
-  public function execute()
+  /**
+   * @description Find PDF files in course materials (not student files, stamps, etc) that haven't already been scanned
+   * @param $conn - The Moodle DB connection object
+   * @return array|bool
+   */
+  private function get_files($conn)
   {
-    global $CFG, $DB;
-    require_once($CFG->libdir . '/filelib.php');
 
-    mtrace("Filescan cron script is running");
+    // Grab some of the plugin variables from the config, we need these while constructing the query
+    $max_files_to_check = (int) get_config("block_filescan", "numfilespercron");
+    $max_retries = (int) get_config("block_filescan", "maxretries");
 
-    $api_url = get_config('block_filescan', 'apiurl');
+    $query = <<<EOQ
+      SELECT DISTINCT f.id, f.contenthash
+      FROM {files} f, {context} c
+      WHERE c.id = f.contextid
+       AND c.contextlevel = 70
+       AND f.filesize <> 0
+       AND f.mimetype = 'application/pdf'
+       AND f.component <> 'assignfeedback_editpdf'
+       AND f.filearea <> 'stamps'
+       AND f.contenthash NOT IN
+           (SELECT contenthash
+             FROM {block_filescan_files}
+             WHERE checked = 1
+                OR (checked = 0 AND status = 'error' AND statuscode >= $max_retries))
+      ORDER BY f.id DESC
+      LIMIT $max_files_to_check; 
+    EOQ;
 
-    // Only proceed if config parameters are defined
-    if (empty($api_url)) {
-      throw new \RuntimeException("Moodle FileScan API URL not configured");
-    }
+    // make the query and return an array of the files
+    $content_hashes = $conn->get_records_sql($query);
 
-    mtrace("Looking up files to scan");
-
-    $max_files_to_check = (int)get_config('block_filescan', 'numfilespercron');
-    $max_files_to_check = (is_int($max_files_to_check) && $max_files_to_check > 0) ? $max_files_to_check : 2;
-
-    $max_retries = (int)get_config('block_filescan', 'maxretries');
-
-    // Find PDF files in course materials (not student files, stamps, etc) that haven't already been scanned
-    // Have to do 2 lookups because there can be multiple entries for each contenthash and need to ensure we get the latest updated contenthashes
-    $query = "SELECT DISTINCT f.id, f.contenthash
-                FROM {files} f, {context} c
-               WHERE c.id = f.contextid
-                 AND c.contextlevel = 70
-                 AND f.filesize <> 0
-                 AND f.mimetype = 'application/pdf'
-                 AND f.component <> 'assignfeedback_editpdf'
-                 AND f.filearea <> 'stamps'
-               ORDER BY f.id DESC
-               LIMIT $max_files_to_check";
-
-//    $query = "SELECT DISTINCT f.id, f.contenthash
-//                FROM {files} f, {context} c
-//               WHERE c.id = f.contextid
-//                 AND c.contextlevel = 70
-//                 AND f.filesize <> 0
-//                 AND f.mimetype = 'application/pdf'
-//                 AND f.component <> 'assignfeedback_editpdf'
-//                 AND f.filearea <> 'stamps'
-//                 AND f.contenthash NOT IN
-//                     (SELECT contenthash
-//                        FROM {block_filescan_files}
-//                       WHERE checked = 1
-//                          OR (checked = 0 AND status = 'error' AND statuscode >= $max_retries))
-//               ORDER BY f.id DESC
-//               LIMIT $max_files_to_check";
-
-    $contenthashes = $DB->get_records_sql($query);
-
-    if (!$contenthashes) {
+    // Only continue if there are content hashes
+    if (!$content_hashes) {
       mtrace("No files found");
       return false;
     }
 
+    // TODO: There's got to be a better way to do this...
     // Set up an array containing the latest contenthashes
     $hash_array = array();
-    foreach ($contenthashes as $c) {
+
+    foreach ($content_hashes as $c) {
       $hash_array[] = $c->contenthash;
     }
-    $comma_separated_contenthashes = implode("','", $hash_array);
-    $comma_separated_contenthashes = "'" . $comma_separated_contenthashes . "'";
 
-    $query = 'SELECT f.contenthash, f.pathnamehash FROM {files} f WHERE f.contenthash IN (' . $comma_separated_contenthashes . ') GROUP BY f.contenthash, f.pathnamehash';
+    $comma_separated_content_hashes = implode("','", $hash_array);
+    $comma_separated_content_hashes = "'" . $comma_separated_content_hashes . "'";
 
-    $files = $DB->get_records_sql($query);
+    $query = <<<EOQ
+      SELECT f.contenthash, f.pathnamehash 
+      FROM {files} f 
+      WHERE f.contenthash IN ($comma_separated_content_hashes)
+      GROUP BY f.contenthash, f.pathnamehash
+    EOQ;
 
-    if (!$files) {
-      mtrace("No files found");
-      return false;
+    $files = $conn->get_records_sql($query);
+
+    // Either return an array of the files, or return an empty array.
+    if ($files) {
+      return $files;
+    } else {
+      mtrace("No files found!");
+      return array();
     }
+  }
+
+  /**
+   * @description Sends supplied files to the filescan server and handles the responses
+   * @param $conn - The Moodle DB connection object
+   * @param $files
+   * @return bool
+   */
+  private function process_requests($conn, $files)
+  {
+
+    // If there are no files found, don't bother proceeding
+    if (sizeof($files) == 0) {
+      return true;
+    }
+
+    // Set up the request headers
+    $headers = array(
+      "cache-control: no-cache",
+      "Content-Type: multipart/form-data",
+      'Accept: application/json'
+    );
+
+    // Set up curl options
+    $curl_opts = array(
+      "curlopt_httpheader" => $headers,
+      "curlopt_timeout" => "120L",
+      "curlopt_followlocation" => true,
+      "curlopt_header" => false
+    );
+
 
     // Get the file storage , we will need this for getting the files by their hash
     $fs = get_file_storage();
 
-    // output the files
-    foreach($files as $f) {
+    // For each file, construct a Moodle curl request
+    foreach ($files as $f) {
+
       $file = $fs->get_file_by_hash($f->pathnamehash);
       $file_contents = $file->get_content();
       $file_content_hash = $f->contenthash;
@@ -136,38 +166,24 @@ class scan_files extends \core\task\scheduled_task
         $row->contenthash = $file_content_hash;
         $row->status = "error";
         $row->checked = 0;
-        self::save_filescan_results($DB, $row);
+        self::save_filescan_results($conn, $row);
         continue;
       }
 
-      // Construct the request
-      $opts = array();
-      $headers = array(
-        "cache-control: no-cache",
-        "Content-Type: multipart/form-data"
-      );
-
-      // See the undocumented curl docs in the moodle source code
+      // Set up the request handler, which is an instance of Moodle's curl implementation
+      // See the undocumented curl docs in the Moodle source code
       // https://github.com/moodle/moodle/blob/master/lib/filelib.php#L2972
+      $request = new \curl();
 
-      $request = new \curl($opts);
-
-      // Make the request and get the response
-      $response = $request->post(
-        $api_url . "/v1/file",
-        array(
-          "upfile" => $file,
-          "id" => $f->contenthash
-        ),
-        array(
-          "CURLOPT_HEADER" => $headers,
-          "CURLOPT_TIMEOUT" => 10000,
-          "CURLOPT_FRESH_CONNECT" => true,
-          "CURLOPT_FOLLOWLOCATION" => true,
-          "CURLOPT_SSL_VERIFYPEER" => false
-        )
+      $params = array(
+        "upfile" => $file,
+        "id" => $f->contenthash
       );
 
+      // Make the request and store it in the response variable
+      $response = $request->post($this->get_filescan_endpoint(), $params, $curl_opts);
+
+      // Get the curl info from the request, we need the http status codes from it
       $request_info = $request->get_info();
 
       // Create the row object here and assign the contenthash to it
@@ -184,34 +200,85 @@ class scan_files extends \core\task\scheduled_task
         // Update the row with an error status and break out of the loop
         $row->status = "error";
         $row->checked = 0;
-        self::save_filescan_results($DB, $row);
+        self::save_filescan_results($conn, $row);
         continue;
       }
 
       // Decode the response into an array of arrays
-      $results  = json_decode($response);
+      $results = json_decode($response, true);
 
       // Handle the results
       $row->timechecked = date("Y-m-d H:i:s");
-      $row->hastext = $results["application/json"]["hasText"];
-      $row->hasTitle = $results["application/json"]["title"];
-      $row->hasLanguage = $results["application/json"]["language"];
-      $row->hasOutline = $results["application/json"]["hasOutline"];
+      $row->hastext = (int) $results["application/json"]["hasText"];
+      $row->hastitle = (int) $results["application/json"]["title"];
+      $row->haslanguage = (int) $results["application/json"]["language"];
+      $row->hasoutline = (int) $results["application/json"]["hasOutline"];
       $row->checked = 1;
-      $row->pagecount = $results["application/json"]["numPages"];
+      $row->pagecount = (int) $results["application/json"]["numPages"];
 
       // Determine if the file is accessible or not
       // no text -> fail
       // has text, title, language, and outline -> pass 🔥🔥🔥
       // has text but is missing other -> check
-      $row->status = ($row->hasTitle && $row->hasLanguage && $row->hasOutline)
-        ? ($row->hasText ? "pass" : "fail")
-        : "check" ;
+      $row->status = ($row->hastitle && $row->haslanguage && $row->hasoutline)
+        ? ($row->hastext ? "pass" : "fail")
+        : "check";
 
       // Update the database with the results
-      self::save_filescan_results($DB, $row);
+      self::save_filescan_results($conn, $row);
 
     }
+
+    return true;
+  }
+
+  /**
+   * @description Returns the filescan server endpoint for scanning a file
+   * @return string
+   */
+  private function get_filescan_endpoint()
+  {
+    $api_url = get_config('block_filescan', 'apiurl');
+    if (empty($api_url)) {
+      // TODO: If a default filescan server URL is every implemented, return it here instead of throwing an error
+      throw new \runtimeexception("Moodle filescan API URL is missing.");
+    } else {
+      return $api_url . "/v1/file";
+    }
+  }
+
+  /**
+   * @description This is a helper function get easily get config variables
+   * @param $var string
+   * @return string
+   */
+  private function get_from_config($var)
+  {
+    $cvar = get_config("block_filescan", (string)$var);
+    if (!$cvar) {
+      throw new \runtimeexception("Oops! It looks like you tried to request a variable from your plugin's configuration that doesn't exist. Be careful!");
+    } else {
+      return $cvar;
+    }
+  }
+
+  /**
+   * @description Runs the scan_files task
+   * @return bool
+   */
+  public function execute()
+  {
+    global $CFG, $DB;
+    require_once($CFG->libdir . '/filelib.php');
+
+    mtrace("Filescan cron script is running.");
+
+    // Get the files and process them
+    $files = self::get_files($DB);
+    self::process_requests($DB, $files);
+
+    mtrace("Done!");
+    return true;
   }
 
 }
--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
 
 $plugin->component = 'block_filescan';
-$plugin->version = 2019100100; //yyyymmdd##
+$plugin->version = 2019100208; //yyyymmdd##
 $plugin->requires = 2016052300;
