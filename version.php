5d2ddd6fb4ead23b7af5a84f560594eff11d5a50
Anthony Weed <devnoot@gmail.com>
2023-02-16 11:41:25 -0500

    Updated version

--- version.php
+++ version.php
@@ -1,5 +1,28 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$plugin->component = 'block_filescan';
-$plugin->version = 2019110901; //yyyymmdd##
-$plugin->requires = 2016052300;
+/**
+* Version information for block_a11y_check
+*
+* @package   block_a11y_check
+* @copyright 2023 Swarthmore College
+* @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
+
+$plugin->component = 'block_a11y_check';
+$plugin->version = 2023021600;
+$plugin->requires = 2022041906.02;
+$plugin->maturity = MATURITY_ALPHA;
